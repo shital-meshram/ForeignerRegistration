695b96c3c39ec3c84da3b592cc15fa6788057a6a
mshital <shital.meshram2330@gmail.com>
2024-05-07 13:56:53 +0530

    initital commit

---
+++ ForeignerRegistration/src/main/java/com/example/ForeignerRegistration/ForeignerRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ForeignerRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignerRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignerRegistrationApplication.class, args);
+	}
+
+}
---
+++ ForeignerRegistration/src/test/java/com/example/ForeignerRegistration/ForeignerRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ForeignerRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignerRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
